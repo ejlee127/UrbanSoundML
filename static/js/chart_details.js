84c4bcad1f8a08d89875d3fc6293efa1f782e2e2
Lyra <63160185+ejlee127@users.noreply.github.com>
2020-12-09 11:23:14 -0500

    updating js

--- static/js/chart_details.js
+++ static/js/chart_details.js
@@ -11,7 +11,45 @@ var classColors = {
     "street_music": "#EDC948"
 }
 
-displayMetadata()
+metadata = class_cnt_in_folds;
+console.log(metadata)
+displayClassCnt();
+
+function displayClassCnt() {
+    classNames = Object.keys(metadata)
+    cnt_datasets = Object.entries(metadata).map( ([cl, cnt]) => {
+      console.log(cl, cnt)
+      var data_one = {
+        'label' : cl,
+        'data' :  cnt,
+        'backgroundColor': classColors[cl]
+      }
+      return data_one
+    });
+    console.log(cnt_datasets);
+    labels = metadata[classNames[0]].map( (d,i) => 'fold '+`${i+1}`);
+    console.log(labels)
+    ctx = document.getElementById('metadata').getContext('2d');
+    var myLineChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets:   cnt_datasets
+      },
+      options: {
+        legend: { 
+            display: true,
+            labels: {
+              boxWidth: 10
+            }
+        },
+        title: {
+            display: true,
+            text: "Classes Count in Folders"
+        },
+      }
+    });
+}
 
 function displayMetadata() {
   url = "/static/result_files/metadata_class_cnt.json"
